c405a524bd9e28092bbc00d97524c1076ce4441a
samtcevik <samtcevik@gmail.com>
2023-08-14 18:21:54 +0300

    Frontend
    Frontend

---
+++ Backend/Activities/Activities.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Activities.Domain.Entities;
+using Activities.Persistence.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Activities.API.Controllers
+{
+
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+        public ActivitiesController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api/activities/{id}
+        public async Task<ActionResult<Activity>> GetActivity(int id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+
+    }
+}
